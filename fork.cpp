0586f64771ed02ac9f69397bd7773882cf6ff7ad
pangjiwei <2581628213@qq.com>
2021-12-17 11:28:08 +0800

    test2

---
+++ fork.cpp
@@ -0,0 +1,17 @@
+/*linux下：*/
+ 
+#include <stdio.h>
+#include <unistd.h>
+#include <windows.h> 
+ 
+int main() {
+    pid_t pid;
+    pid = fork();
+    if(pid  == 0) //返回子进程
+    {
+        printf("child pid: %d\n", getpid());
+    } else {
+        printf("pid: %d\n", pid);//父进程中返回子进程的pid
+        printf("father pid: %d\n", getpid());
+    }
+}
